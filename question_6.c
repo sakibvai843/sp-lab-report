d511d5d31d22c48e47bb6297384cbeb6ad743778
sakibvai843 <sanju84143@gmail.com>
2024-06-04 06:45:24 +0600

    Create question_6.c

---
+++ question_6.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Function to check if a number is prime
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    if (n <= 3) {
+        return true;
+    }
+    if (n % 2 == 0 || n % 3 == 0) {
+        return false;
+    }
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int n;
+
+    // Prompt the user to enter an integer
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    // Check if the number is prime and display the result
+    if (isPrime(n)) {
+        printf("%d is a prime number.\n", n);
+    } else {
+        printf("%d is not a prime number.\n", n);
+    }
+
+    return 0;
+}
