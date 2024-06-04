953537999f2fea84d309a70d57ec796d93a9c080
sakibvai843 <sanju84143@gmail.com>
2024-06-04 06:44:12 +0600

    Create question_5.c

---
+++ question_5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// Function to check if a string is a palindrome
+int isPalindrome(char str[]) {
+    int left = 0;
+    int right = strlen(str) - 1;
+
+    while (left < right) {
+        // Skip non-alphanumeric characters
+        while (left < right && !isalnum(str[left])) {
+            left++;
+        }
+        while (left < right && !isalnum(str[right])) {
+            right--;
+        }
+        // Compare characters
+        if (tolower(str[left]) != tolower(str[right])) {
+            return 0;
+        }
+        left++;
+        right--;
+    }
+    return 1;
+}
+
+int main() {
+    char str[100];
+
+    // Prompt the user to enter a string
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove newline character if present
+    str[strcspn(str, "\n")] = '\0';
+
+    // Check if the string is a palindrome
+    if (isPalindrome(str)) {
+        printf("The string is a palindrome.\n");
+    } else {
+        printf("The string is not a palindrome.\n");
+    }
+
+    return 0;
+}
